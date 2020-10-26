cb6d161084b5a201bc7b165490635c13006f941d
Yoshimine Sato <universato@gmail.com>
2020-10-26 20:41:13 +0900

    add ToDo

---
+++ main.js
@@ -0,0 +1,62 @@
+var STORAGE_KEY = 'todos-vuejs-3.0';
+
+var todoStorage = {
+  fetch: function() {
+    var todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
+    todos.forEach(function(todo, index) {
+      todo.id = index;
+    });
+    todoStorage.uid = todos.length;
+    return todos;
+  },
+  save: function(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+  }
+};
+
+let app = Vue.createApp({
+  data() {
+    return {
+      // todos: [{completed: true, title: 'hoge'}, ......]
+      todos: todoStorage.fetch(),
+      newTodo: '',
+    }
+  },
+  methods: {
+    addTodo: function() {
+      let value = this.newTodo && this.newTodo.trim();
+      if (!value) { return; }
+      this.todos.push({
+        id: todoStorage.uid++,
+        title: value,
+        completed: false
+      });
+      this.newTodo = '';
+    },
+    removeTodo: function(todo) {
+      this.todos.splice(this.todos.indexOf(todo), 1);
+    },
+  },
+  watch: {
+    todos: {
+      handler: function(todos) {
+        todoStorage.save(todos);
+      },
+      deep: true
+    }
+  },
+  computed: {
+    numberOfTodos: function(){
+      return this.todos.length;
+    },
+    numberOfCompletions: function(){
+      let count = 0;
+      for(let i = 0; i < this.todos.length; i++){
+        if(this.todos[i].completed){ count++; }
+      }
+      return count;
+    },
+  }
+})
+
+app.mount('#todoapp')
